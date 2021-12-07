3d46946a96f379d35d975f477f9bb747fe985f81
louygan <louygan@users.noreply.github.com>
2021-12-07 12:59:49 +0800

    Update main.js
    fix logical error

--- js/main.js
+++ js/main.js
@@ -259,16 +259,16 @@ async function fetchForkInfo(repo, ownerAndBranch, forkOwner, forkBranch) {
     })
     .then(data => {
       console.log(data);
-      if ( data.status.localeCompare("identical") = 0 ) {
+      if ( data.status.localeCompare("identical") != 0 ) {
+        if ( data.status.localeCompare("ahead") != 0 ) {
+          return "behind+" + data.behind_by;   
+        else {
+          return "ahead+" + data.ahead_by;   
+        }
+      }
+      else {
         return "identical+0";
       }
-      if ( data.status.localeCompare("ahead") = 0 ) {
-        return "ahead+" + data.ahead_by;   
-      }
-      if ( data.status.localeCompare("behind") = 0 ) {
-        return "behind+" + data.behind_by;   
-      }
-      return "unknown";
     })
     .catch(error => {
       const msg =
